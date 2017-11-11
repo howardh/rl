4aaad116a1253309ad9d9dc1155b307ecb4ca2ad
Howard HUANG <hhuang63@CS.McGill.CA>
2017-11-10 19:07:04 -0500

    Grid search implemented for Mountain Car RBFs

--- mainmc.py
+++ mainmc.py
@@ -3,9 +3,14 @@ import numpy as np
 import datetime
 import dill
 import os
+import itertools
+from tqdm import tqdm
+import time
+
 
 from agent.discrete_agent import TabularAgent
 from agent.lstd_agent import LSTDAgent
+from agent.rbf_agent import RBFAgent
 
 import mountaincar 
 import mountaincar.features
@@ -69,10 +74,13 @@ def parse_results(directory):
     for file_name in file_names:
         with open(os.path.join(directory,file_name), 'rb') as f:
             x = dill.load(f)
+            diverged = None in x[1]
+            if diverged:
+                x[1] = [a for a in x[1] if a is not None]
             if x[2] is None:
-                results.append((x[0], file_name, np.mean(x[1]), np.inf))
+                results.append((x[0], file_name, np.mean(x[1]), np.inf, diverged))
             else:
-                results.append((x[0], file_name, np.mean(x[1]), x[2]))
+                results.append((x[0], file_name, np.mean(x[1]), x[2], diverged))
     print("\nSorting by mean reward...")
     results.sort(key=lambda x: x[2], reverse=True)
     for i in range(min(10,len(results))):
@@ -80,6 +88,7 @@ def parse_results(directory):
         print("\t%s" % results[i][0])
         print("\t%s" % results[i][1])
         print("\t%s" % results[i][3])
+        print("\tDiverged?: %s" % results[i][4])
 
     print("\nSorting by time to learn...")
     results.sort(key=lambda x: x[3], reverse=True)
@@ -88,6 +97,94 @@ def parse_results(directory):
         print("\t%s" % results[i][0])
         print("\t%s" % results[i][1])
         print("\t%s" % results[i][2])
+        print("\tDiverged?: %s" % results[i][4])
+
+def rbf_control(discount_factor, learning_rate, initial_value, num_pos,
+        num_vel, behaviour_eps, target_eps, epoch, max_iters, test_iters,
+        results_dir):
+    args = locals()
+    print(args)
+    env_name = 'MountainCar-v0'
+    e = gym.make(env_name)
+    start_time = datetime.datetime.now()
+
+    action_space = np.array([0,1,2])
+    obs_space = np.array([[-1.2, .6], [-0.07, 0.07]])
+    def norm(x):
+        return np.array([(s-r[0])/(r[1]-r[0]) for s,r in zip(x, obs_space)])
+    agent = RBFAgent(
+            action_space=action_space,
+            observation_space=np.array([[0,1],[0,1]]),
+            discount_factor=discount_factor,
+            learning_rate=learning_rate,
+            initial_value=initial_value,
+            features=norm
+    )
+    agent.set_behaviour_policy("%f-epsilon" % behaviour_eps)
+    agent.set_target_policy("%f-epsilon" % target_eps)
+
+    rewards = []
+    steps_to_learn = None
+    iters = 0
+    try:
+        while iters < max_iters:
+            iters += 1
+            r,s = agent.run_episode(e)
+            rewards.append(r)
+            #print("... %d\r" % iters, end='')
+            #if r != -200:
+            #    print("Iteration: %d\t Reward: %d"%(iters, r))
+            if epoch is not None and iters % epoch == 0:
+                print("Testing...")
+                #print(agent.learner.weights.transpose())
+                rewards = agent.test(e, test_iters, render=False, processors=1)
+                #print("Iteration %d\t Rewards: %f" % (iters, np.mean(rewards)))
+                if np.mean(rewards) >= -110:
+                    if steps_to_learn is None:
+                        steps_to_learn = iters
+    except ValueError:
+        print("Diverged")
+        pass # Diverged weights
+
+    while iters < max_iters: # Means it diverged at some point
+        rewards.append(None)
+
+    data = (args, rewards, steps_to_learn)
+    file_name, file_num = utils.find_next_free_file("results", "pkl", results_dir)
+    with open(file_name, "wb") as f:
+        dill.dump(data, f)
+
+    return rewards,steps_to_learn
+
+def gs_rbf(proc=10):
+    #def rbf_control(discount_factor, learning_rate, initial_value, num_pos,
+    #        num_vel, behaviour_eps, target_eps, epoch, max_iters, test_iters):
+    d = [1, 0.9]
+    lr = [1, 0.7, 0.5, 0.3, 0.1, 0.05, 0.01]
+    iv = [0]
+    np = [8]
+    nv = [8]
+    be = [0]
+    te = [0]
+    e = [None]
+    mi = [3000]
+    ti = [0]
+    rd = ["./results-rbf"]
+    indices = itertools.product(d,lr,iv,np,nv,be,te,e,mi,ti, rd)
+
+    futures = []
+    from concurrent.futures import ProcessPoolExecutor
+    with ProcessPoolExecutor(max_workers=proc) as executor:
+        for i in tqdm(indices, desc="Adding jobs"):
+            print(i)
+            future = [executor.submit(rbf_control, *i)]
+            futures += future
+        pbar = tqdm(total=len(futures), desc="Job completion")
+        while len(futures) > 0:
+            count = [f.done() for f in futures].count(True)
+            pbar.update(count)
+            futures = [f for f in futures if not f.done()]
+            time.sleep(1)
 
 def lstd_control():
     env_name = 'MountainCar-v0'
@@ -181,6 +278,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Boo")
     parser.add_argument("--parse-results",
             action="store_true")
+    parser.add_argument("--grid-search",
+            action="store_true")
+    parser.add_argument("--model",
+            type=str, default="rbf", help="Model to use (tabular|rbf)")
     parser.add_argument("--learning-rate",
             type=float, default=0.5, help="Learning rate")
     parser.add_argument("--discount",
@@ -205,22 +306,37 @@ if __name__ == "__main__":
             type=str, default="./results",
             help="Directory in which to place the pickled results")
     args = parser.parse_args()
-    print(args)
 
     if args.parse_results:
         parse_results(args.results_dir)
+    elif args.grid_search:
+        gs_rbf()
     else:
-        r,s = tabular_control(discount_factor=args.discount,
-                learning_rate=args.learning_rate,
-                initial_value=args.initial_value,
-                num_pos=args.num_pos,
-                num_vel=args.num_vel,
-                behaviour_eps=args.behaviour_epsilon,
-                target_eps=args.target_epsilon,
-                epoch=args.epoch,
-                test_iters=args.test_iters,
-                max_iters=args.max_iters)
-        data = (args, r, s)
-        file_name, file_num = utils.find_next_free_file("results", "pkl", args.results_dir)
-        with open(file_name, "wb") as f:
-            dill.dump(data, f)
+        if args.model == "tabular":
+            r,s = tabular_control(discount_factor=args.discount,
+                    learning_rate=args.learning_rate,
+                    initial_value=args.initial_value,
+                    num_pos=args.num_pos,
+                    num_vel=args.num_vel,
+                    behaviour_eps=args.behaviour_epsilon,
+                    target_eps=args.target_epsilon,
+                    epoch=args.epoch,
+                    test_iters=args.test_iters,
+                    max_iters=args.max_iters,
+                    results_dir=args.results_dir)
+        elif args.model == "rbf":
+            r,s = rbf_control(discount_factor=args.discount,
+                    learning_rate=args.learning_rate,
+                    initial_value=args.initial_value,
+                    num_pos=args.num_pos,
+                    num_vel=args.num_vel,
+                    behaviour_eps=args.behaviour_epsilon,
+                    target_eps=args.target_epsilon,
+                    epoch=args.epoch,
+                    test_iters=args.test_iters,
+                    max_iters=args.max_iters,
+                    results_dir=args.results_dir)
+        #data = (args, r, s)
+        #file_name, file_num = utils.find_next_free_file("results", "pkl", args.results_dir)
+        #with open(file_name, "wb") as f:
+        #    dill.dump(data, f)
--- mainmc.sh
+++ mainmc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-lr=(1 0.5 0.2)
+lr=(1 0.7 0.5 0.3 0.2 0.1 0.05 0.01)
 d=(1 0.9)
-n=(4 8 10 16)
-iv=(-1 0 1 -100 100)
+#n=(4 8 10 16)
+n=(8)
+#iv=(-1 0 1 -100 100)
+iv=(0)
 
 #parallel python3 mainmc.py --learning-rate {1} --discount {2} --behaviour-epsilon 0 --target-epsilon 0 --num-pos {3} --num-vel {3} --initial-value {4} ::: ${lr[*]} ::: ${d[*]} ::: ${n[*]} ::: ${iv[*]}
-parallel python3 mainmc.py --learning-rate 0.3 --discount {1} --behaviour-epsilon 0 --target-epsilon 0 --num-pos {2} --num-vel {2} --initial-value {3} ::: ${d[*]} ::: ${n[*]} ::: ${iv[*]}
+#parallel python3 mainmc.py --learning-rate 0.3 --discount {1} --behaviour-epsilon 0 --target-epsilon 0 --num-pos {2} --num-vel {2} --initial-value {3} --model rbf --results-dir ./results-rbf ::: ${d[*]} ::: ${n[*]} ::: ${iv[*]}
+parallel python3 mainmc.py --learning-rate {1} --discount {2} --behaviour-epsilon 0 --target-epsilon 0 --num-pos {3} --num-vel {3} --initial-value {4} --model rbf --results-dir ./results-rbf ::: ${lr[*]} ::: ${d[*]} ::: ${n[*]} ::: ${iv[*]}
