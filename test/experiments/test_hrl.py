9cbefe53e32de2e4e2a246cf63b011766dd004fa
Howard <howard.huang@mail.mcgill.ca>
2019-08-08 17:55:31 -0400

    Fixed tests

--- test/experiments/test_hrl.py
+++ test/experiments/test_hrl.py
@@ -1,6 +1,6 @@
 import pytest
 
-from experiments.hrl.main import run_trial_steps as run_trial, run_trial_steps
+from experiments.hrl.gridsearch import run_trial
 
 def test_run_trial_steps(tmp_path):
-    run_trial_steps(gamma=0.9,alpha=1e-3,eps_b=0.1,eps_t=0,tau=1,directory=tmp_path,env_name='FrozenLake-v0',max_steps=10,epoch=2,min_replay_buffer_size=1,verbose=True)
+    run_trial(gamma=0.9,alpha=1e-3,eps_b=0.1,eps_t=0,tau=1,directory=tmp_path,env_name='FrozenLake-v0',max_steps=10,epoch=2,min_replay_buffer_size=1,verbose=True)
