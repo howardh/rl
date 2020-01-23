5d0be4a13f7d15faf22197fed2e0c9cde6bab2eb
Howard Huang <howard.huang@mail.mcgill.ca>
2020-01-23 16:47:27 -0500

    Test fix

--- test/experiments/test_hrl.py
+++ test/experiments/test_hrl.py
@@ -18,4 +18,28 @@ def test_transfer(tmp_path):
     results_directory = os.path.join(tmp_path,'results')
     plot_directory = os.path.join(tmp_path,'plot')
 
-    run_trial(gamma=0.9,agent_name='ActorCritic',steps_per_task=3,total_steps=12,epoch=3,test_iters=1,verbose=True,directory=results_directory,controller_net_structure=(),subpolicy_net_structure=(),q_net_structure=())
+    params = {
+        'agent_name': 'ActorCritic',
+        'gamma': 0.9,
+        'controller_learning_rate': 1e-3,
+        'subpolicy_learning_rate': 1e-3,
+        'q_net_learning_rate': 1e-3,
+        'eps_b': 0,
+        'polyak_rate': 0.001,
+        'batch_size': 2,
+        'min_replay_buffer_size': 5,
+        'steps_per_task': 3,
+        'total_steps': 12,
+        'epoch': 3,
+        'test_iters': 1,
+        'verbose': True,
+        'num_options': 2,
+        'cnet_n_layers': 0,
+        'cnet_layer_size': 0,
+        'snet_n_layers': 0,
+        'snet_layer_size': 0,
+        'qnet_n_layers': 0,
+        'qnet_layer_size': 1,
+        'directory': results_directory
+    }
+    run_trial(**params)
