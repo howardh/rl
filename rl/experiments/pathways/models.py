7aebea9baaf75a1216b0438a5a4953c5fa81dfa7
Howard <howard.huang@mail.mcgill.ca>
2022-01-27 10:54:12 -0500

    Pathways

---
+++ rl/experiments/pathways/models.py
@@ -0,0 +1,232 @@
+import torch
+from torchtyping.tensor_type import TensorType
+from torch.utils.data.dataloader import default_collate
+
+from rl.agent.smdp.a2c import PolicyValueNetworkRecurrent
+
+
+class RecurrentAttention(torch.nn.Module):
+    def __init__(self, input_size, key_size, value_size, num_heads, ff_size):
+        super(RecurrentAttention, self).__init__()
+        self.fc_query = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_key = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_value = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, value_size)
+        )
+        self.attention = torch.nn.MultiheadAttention(key_size, num_heads=num_heads, batch_first=False)
+    def forward(self,
+            x: TensorType['batch_size','input_size',float],
+            input_keys: TensorType['seq_len','batch_size','key_size',float],
+            input_values: TensorType['seq_len','batch_size','value_size',float]):
+        query = self.fc_query(x).unsqueeze(0) # (1, batch_size, key_size)
+        attn_output, attn_output_weights = self.attention(query, input_keys, input_values) # (1, batch_size, value_size)
+        output_keys = self.fc_key(attn_output) # (1, batch_size, key_size)
+        output_values = self.fc_value(attn_output) # (1, batch_size, value_size)
+        return {
+            'attn_output': attn_output.squeeze(0), # (batch_size, value_size)
+            'attn_output_weights': attn_output_weights.squeeze(1), # (batch_size, seq_len)
+            'key': output_keys.squeeze(0), # (batch_size, key_size)
+            'value': output_values.squeeze(0), # (batch_size, value_size)
+            'x': attn_output.squeeze(0), # (batch_size, value_size)
+        }
+
+
+class RecurrentAttention2(torch.nn.Module):
+    def __init__(self, input_size, key_size, value_size, num_heads, ff_size):
+        super(RecurrentAttention2, self).__init__()
+        self.fc_query = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_key = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_value = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, value_size)
+        )
+        self.fc_output = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size*2, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, input_size)
+        )
+        self.attention = torch.nn.MultiheadAttention(key_size, num_heads=num_heads, batch_first=False)
+    def forward(self,
+            x: TensorType['batch_size','input_size',float],
+            input_keys: TensorType['seq_len','batch_size','key_size',float],
+            input_values: TensorType['seq_len','batch_size','value_size',float]):
+        query = self.fc_query(x).unsqueeze(0) # (1, batch_size, key_size)
+        attn_output, attn_output_weights = self.attention(query, input_keys, input_values) # (1, batch_size, value_size)
+        output_keys = self.fc_key(attn_output) # (1, batch_size, key_size)
+        output_values = self.fc_value(attn_output) # (1, batch_size, value_size)
+        output_x = self.fc_output(torch.cat([attn_output.squeeze(0),x], dim=1)) # (batch_size, value_size)
+        return {
+            'attn_output': attn_output.squeeze(0), # (batch_size, value_size)
+            'attn_output_weights': attn_output_weights.squeeze(1), # (batch_size, seq_len)
+            'key': output_keys.squeeze(0), # (batch_size, key_size)
+            'value': output_values.squeeze(0), # (batch_size, value_size)
+            'x': output_x # (batch_size, value_size)
+        }
+
+
+class RecurrentAttention3(torch.nn.Module):
+    def __init__(self, input_size, key_size, value_size, num_heads, ff_size):
+        super(RecurrentAttention3, self).__init__()
+        self.fc_query = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_key = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, key_size)
+        )
+        self.fc_value = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, value_size)
+        )
+        self.fc_output = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size*2, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, input_size)
+        )
+        self.fc_gate = torch.nn.Sequential(
+                torch.nn.ReLU(),
+                torch.nn.Linear(input_size*2, ff_size),
+                torch.nn.ReLU(),
+                torch.nn.Linear(ff_size, 1),
+                torch.nn.Sigmoid()
+        )
+        self.attention = torch.nn.MultiheadAttention(key_size, num_heads=num_heads, batch_first=False)
+    def forward(self,
+            x: TensorType['batch_size','input_size',float],
+            input_keys: TensorType['seq_len','batch_size','key_size',float],
+            input_values: TensorType['seq_len','batch_size','value_size',float]):
+        query = self.fc_query(x).unsqueeze(0) # (1, batch_size, key_size)
+        attn_output, attn_output_weights = self.attention(query, input_keys, input_values) # (1, batch_size, value_size)
+        output_keys = self.fc_key(attn_output) # (1, batch_size, key_size)
+        output_values = self.fc_value(attn_output) # (1, batch_size, value_size)
+        output_x = self.fc_output(torch.cat([attn_output.squeeze(0),x], dim=1)) # (batch_size, value_size)
+        output_gate = self.fc_gate(torch.cat([attn_output.squeeze(0),x], dim=1)) # (batch_size, 1)
+        return {
+            'attn_output': attn_output.squeeze(0), # (batch_size, value_size)
+            'attn_output_weights': attn_output_weights.squeeze(1), # (batch_size, seq_len)
+            'key': output_keys.squeeze(0), # (batch_size, key_size)
+            'value': output_values.squeeze(0), # (batch_size, value_size)
+            'x': output_gate*output_x + (1-output_gate)*attn_output.squeeze(0) # (batch_size, value_size)
+        }
+
+
+class ConvPolicy(PolicyValueNetworkRecurrent):
+    def __init__(self, num_actions, in_channels, input_size, key_size, value_size, num_heads, ff_size, num_blocks=1):
+        super(ConvPolicy, self).__init__()
+        self.key_size = key_size
+        self.input_size = input_size
+
+        self.conv = torch.nn.Sequential(
+            torch.nn.Conv2d(
+                in_channels=in_channels,out_channels=32,kernel_size=8,stride=4),
+            torch.nn.LeakyReLU(),
+            torch.nn.Conv2d(
+                in_channels=32,out_channels=64,kernel_size=4,stride=2),
+            torch.nn.LeakyReLU(),
+            torch.nn.Conv2d(
+                in_channels=64,out_channels=64,kernel_size=3,stride=1),
+            torch.nn.Flatten(),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(in_features=64*7*7,out_features=512),
+            torch.nn.LeakyReLU(),
+        )
+        self.fc_key = torch.nn.Linear(in_features=512, out_features=key_size)
+        self.fc_value = torch.nn.Linear(in_features=512, out_features=value_size)
+        self.attention = torch.nn.ModuleList([
+                RecurrentAttention(input_size, key_size, value_size, num_heads, ff_size)
+                for _ in range(num_blocks)
+        ])
+
+        self.fc_output = torch.nn.Sequential(
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(input_size, 512),
+                torch.nn.LeakyReLU(),
+        )
+        self.v = torch.nn.Linear(in_features=512,out_features=1)
+        self.pi = torch.nn.Linear(in_features=512,out_features=num_actions)
+
+    def forward(self, x: TensorType['batch_size','observation_shape'], hidden):
+        batch_size = x.shape[0]
+        device = next(self.parameters()).device
+
+        x = self.conv(x)
+        keys = torch.cat([
+            self.fc_key(x).unsqueeze(0),
+            hidden[0]
+        ], dim=0)
+        values = torch.cat([
+            self.fc_value(x).unsqueeze(0),
+            hidden[1]
+        ], dim=0)
+
+        new_hidden = []
+        x = torch.zeros([batch_size, self.input_size], device=device)
+        for attention in self.attention:
+            output = attention(x, keys, values)
+            x = output['x']
+            new_hidden.append((output['key'], output['value']))
+        x = self.fc_output(x)
+
+        return {
+            'value': self.v(x),
+            'action': self.pi(x),
+            'hidden': default_collate(new_hidden)
+        }
+
+    def init_hidden(self, batch_size: int = 1):
+        device = next(self.parameters()).device
+        return (
+                torch.zeros([len(self.attention), batch_size, self.key_size], device=device), # Key
+                torch.zeros([len(self.attention), batch_size, self.key_size], device=device), # Query
+        )
+
+
+
+if __name__ == '__main__':
+    rann = RecurrentAttention(input_size=10, key_size=10, value_size=10, num_heads=2, ff_size=10)
+    out = rann(
+        torch.randn(2, 10),
+        torch.randn(10, 2, 10),
+        torch.randn(10, 2, 10)
+    )
+
+    net = ConvPolicy(num_actions=6, in_channels=4, input_size=10, key_size=10, value_size=10, num_heads=2, ff_size=10)
+    out = net(
+        torch.randn(2, 4, 84, 84),
+        net.init_hidden(2)
+    )
+    breakpoint()
---
+++ rl/experiments/pathways/train.py
@@ -0,0 +1,169 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+import gym.spaces
+import dill
+import experiment.logger
+from experiment import load_checkpoint, make_experiment_runner
+from experiment.logger import Logger
+
+from rl.agent.smdp.a2c import A2CAgentRecurrentVec, PolicyValueNetworkRecurrent
+from rl.experiments.training.vectorized import TrainExperiment
+from rl.experiments.recurrence.train import ExperimentConfigs
+from rl.experiments.pathways.models import ConvPolicy
+
+
+class Agent(A2CAgentRecurrentVec):
+    def _init_default_net(self, observation_space, action_space, device) -> PolicyValueNetworkRecurrent:
+        if isinstance(observation_space, gym.spaces.Box):
+            assert observation_space.shape is not None
+            if len(observation_space.shape) == 1: # Mujoco
+                raise Exception('Unsupported observation space or action space.')
+            if len(observation_space.shape) == 3: # Atari
+                return ConvPolicy(
+                        num_actions=action_space.n,
+                        input_size=512,
+                        key_size=512,
+                        value_size=512,
+                        num_heads=8,
+                        ff_size = 1024,
+                        in_channels=observation_space.shape[0],
+                ).to(device)
+        raise Exception('Unsupported observation space or action space.')
+
+
+def get_params():
+    from rl.experiments.pathways.train import Agent # Need to import for pickling purposes
+
+    params = ExperimentConfigs()
+
+    num_envs = 16
+    env_name = 'Pong-v5'
+    #env_config = {
+    #    'frameskip': 1,
+    #    'mode': 0,
+    #    'difficulty': 0,
+    #    'repeat_action_probability': 0.25,
+    #}
+    env_config = {
+            'env_name': env_name,
+            'atari': True,
+            'atari_config': {
+                'num_envs': num_envs,
+                'stack_num': 1,
+                'repeat_action_probability': 0.25,
+            }
+    }
+
+    params.add('exp-001', {
+        'agent': {
+            'type': Agent,
+            'parameters': {
+                'target_update_frequency': 32_000,
+                'num_train_envs': num_envs,
+                'num_test_envs': 1,
+                'max_rollout_length': 128,
+                'hidden_reset_min_prob': 0,
+                'hidden_reset_max_prob': 0,
+            },
+        },
+        'env_test': env_config,
+        'env_train': env_config,
+        'test_frequency': None,
+        'save_model_frequency': None,
+        'verbose': True,
+    })
+
+    return params
+
+
+def make_app():
+    import typer
+    app = typer.Typer()
+
+    @app.command()
+    def run(exp_name : str,
+            trial_id : Optional[str] = None,
+            results_directory : Optional[str] = None,
+            slurm : bool = typer.Option(False, '--slurm'),
+            wandb : bool = typer.Option(False, '--wandb'),
+            debug : bool = typer.Option(False, '--debug')):
+        config = get_params()[exp_name]
+        if debug:
+            raise NotImplementedError()
+        else:
+            exp_runner = make_experiment_runner(
+                    TrainExperiment,
+                    config=config,
+                    results_directory=results_directory,
+                    trial_id=trial_id,
+                    checkpoint_frequency=250_000,
+                    max_iterations=5_000_000,
+                    slurm_split=slurm,
+                    verbose=True,
+                    modifiable=True,
+            )
+            if wandb:
+                exp_runner.exp.logger.init_wandb({
+                    'project': f'A2C-pathways-{exp_name}'
+                })
+        exp_runner.run()
+        exp_runner.exp.logger.finish_wandb()
+
+    @app.command()
+    def checkpoint(filename):
+        exp = load_checkpoint(TrainExperiment, filename)
+        exp.run()
+
+    @app.command()
+    def plot(result_directory : Path):
+        import experiment.plotter as eplt
+        from experiment.plotter import EMASmoothing
+
+        checkpoint_filename = os.path.join(result_directory,'checkpoint.pkl')
+        with open(checkpoint_filename,'rb') as f:
+            x = dill.load(f)
+        logger = Logger()
+        logger.load_state_dict(x['exp']['logger'])
+        if isinstance(logger.data, experiment.logger.FileBackedList):
+            logger.data.iterate_past_end = True
+        output_directory = x['exp']['output_directory']
+        plot_directory = os.path.join(output_directory,'plots')
+        os.makedirs(plot_directory,exist_ok=True)
+
+        for k in ['agent_train_state_value_target_net', 'agent_train_state_value', 'train_reward', 'reward']:
+            try:
+                filename = os.path.abspath(os.path.join(plot_directory,f'plot-{k}.png'))
+                eplt.plot(logger,
+                        filename=filename,
+                        curves=[{
+                            'key': k,
+                            'smooth_fn': EMASmoothing(0.9),
+                        }],
+                        min_points=2,
+                        xlabel='Steps',
+                        ylabel=k,
+                        aggregate='mean',
+                        show_unaggregated=False,
+                )
+                print(f'Plot saved to {filename}')
+            except KeyError:
+                print(f'Could not plot {k}. Key not found.')
+
+    commands = {
+            'run': run,
+            'checkpoint': checkpoint,
+            'plot': plot,
+    }
+
+    return app, commands
+
+
+def run():
+    app,_ = make_app()
+    app()
+
+
+if __name__ == "__main__":
+    run()
